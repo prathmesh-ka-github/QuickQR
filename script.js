fd9699ea36a976482d976e67be05a4210b258423
prathmesh-ka-github <pkale@albany.edu>
2025-07-12 21:37:25 -0400

    :sparkles: added file format option

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log("JS working")
 function reveal() {
     let oContainer = document.getElementById('output-container')
     oContainer.style.height ="212px"
@@ -6,15 +5,15 @@ function reveal() {
 async function genqr() {
     let input = document.getElementById('input').value
     let output = document.getElementById('output')
-    let apistring = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + input
+    const fileType = document.getElementById('fileType').value
+    let apistring = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&format=${fileType}&data=${input}`
     
     if (input === "") {
-        output.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=https://github.com/prathmesh-ka-github"
+        output.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&format=${fileType}&data=https://github.com/prathmesh-ka-github`
     } else {
         output.src = apistring
     }
     setTimeout(reveal(), 5000);
-    // oContainer.style.height ="212px"
 }
 
 if (window.innerWidth<600) {
